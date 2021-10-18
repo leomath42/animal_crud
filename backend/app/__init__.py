0627c386017cebb9309e1f37bd3802073a3b9c18
leomath42 <leonardomatheus042@gmail.com>
2021-10-18 07:42:46 -0300

    enviroment variables

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from app.resource.resource import animal
 from flask_cors import CORS
+# import dnspython
+import pymongo
+import os
+
 #from flask_marshmallow import Marshmallow
 
 database = MongoEngine()
@@ -10,13 +14,27 @@ cors = CORS()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "SECRET_KEY"
+    app.secret_key = os.getenv('SECRET_KEY') if os.getenv(
+        'SECRET_KEY') is not None else os.urandom(32)
     #app.config["APPLICATION_ROOT"] = "/api"
-    app.config['MONGODB_SETTINGS'] = {
-        'db': 'local',
-        'host': 'mongodb://localhost/local',
-        "alias": "default",
-    }
+
+    if os.getenv("FLASK_ENV") == "production":
+        assert os.getenv(
+            "MONGO_URI_PRODUCTION") != None, f"MONGO_URI_PRODUCTION VAR NOT DEFINED"
+
+        MONGO_URI_PRODUCTION = os.getenv("MONGO_URI_PRODUCTION")
+
+        app.config['MONGODB_SETTINGS'] = {
+            'connect': 'false',
+            'host': MONGO_URI_PRODUCTION
+        }
+    else:
+        app.config['MONGODB_SETTINGS'] = {
+            'db': 'local',
+            'host': 'mongodb://localhost/local',
+            "alias": "default",
+        }
+
     cors.init_app(app)
     return app
 
