50817a0aa528ee86ff1e2c1d7eb824421650ed5d
leomath42 <leonardomatheus042@gmail.com>
2021-11-23 13:15:50 -0300

    Corrigindo testes unitários #2

--- backend/test/unit_test.py
+++ backend/test/unit_test.py
@@ -1,24 +1,23 @@
 from http import HTTPStatus
 import unittest
 import unittest.mock
-from unittest.mock import patch
-from app.resource.resource import animal
+from unittest.mock import MagicMock, Mock, patch
+
 from flask import Flask, request, jsonify
 from app import create_app, registre_blueprints, init_database
 from app.model.model import Animal
-from mongoengine.errors import DoesNotExist
+from mongoengine.errors import DoesNotExist, ValidationError
 
 from werkzeug.test import Client
 from werkzeug.testapp import test_app
 from mongoengine import connect, disconnect
 from datetime import date
 from bson.objectid import ObjectId
-
+from app.resource.resource import delete
 import json
 
 
-@patch('app.service.service.AnimalService')
-class Test(unittest.TestCase):
+class TestResource(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -29,75 +28,70 @@ class Test(unittest.TestCase):
     def tearDownClass(cls) -> None:
         disconnect()
 
-    def when_id_side_effect(self):
-        existing_id = ObjectId('619bd05b20c5f552ce95e28a')
-        not_valid_id = 0
-        does_not_exist_id = ObjectId('619bd70920c5f552ce95e28b')
+    def setUp(self) -> None:
+        self.id_exist = '619bd05b20c5f552ce95e28a'
+        self.id_does_not_exist = '619bd70920c5f552ce95e28b'
+        self.id_invalid = 0
 
         def side_effect(id):
-            if type(ObjectId(id)) != ObjectId:
-                raise Exception()
-            elif ObjectId(id) == existing_id:
+            if id == self.id_exist:
                 return Animal()
-            elif ObjectId(id) != existing_id:
-                raise DoesNotExist('')
-            else:
-                raise Exception()
+            elif id == self.id_does_not_exist:
+                raise DoesNotExist()
 
-        return side_effect
+        def dump_side_effect(animal: Animal) -> dict:
+            return dict()
 
-    def test_get_animal_when_id_does_exist(self, animal_service_mock) -> None:
-        existing_id = '619bd05b20c5f552ce95e28a'
+        self.animal_service = MagicMock()
+        self.animal_service.delete.side_effect = side_effect
+        self.animal_service.find_by_id.side_effect = side_effect
 
-        animal_service_mock.find_by_id.side_effect = self.when_id_side_effect()
+        self.animal_service.dump.side_effect = dump_side_effect
 
-        with self.__class__.app.test_client() as c, self.__class__.app.container.animal_service.override(animal_service_mock):
+        self.app = self.build_app(self.animal_service)
 
-            response = c.get('/animal/{0}'.format(existing_id))
+    def build_app(self, animal_service: MagicMock) -> Flask:
+        import app.resource.resource as resource
+        resource.animal_service = animal_service
+
+        app = create_app()
+        app.register_blueprint(resource.animal)
+
+        return app
+
+    def test_get_animal_when_id_exist(self) -> None:
+
+        with self.app.test_client() as c:
+
+            response = c.get('/animal/{0}'.format(self.id_exist))
             data = response.data
+            self.animal_service.find_by_id.assert_called_once()
             self.assertEqual(response.status_code, HTTPStatus.OK)
             self.assertIsNotNone(data)
 
-    def test_get_animal_when_id_does_not_exist(self, animal_service_mock) -> None:
-        does_not_exist_id = '619bd70920c5f552ce95e28b'
+    def test_get_animal_when_id_not_exist(self) -> None:
 
-        animal_service_mock.find_by_id.side_effect = self.when_id_side_effect()
+        with self.app.test_client() as c:
 
-        with self.__class__.app.test_client() as c, self.__class__.app.container.animal_service.override(animal_service_mock):
-
-            response = c.get('/animal/{0}'.format(does_not_exist_id))
+            response = c.get('/animal/{0}'.format(self.id_does_not_exist))
             data = response.data
+            self.animal_service.find_by_id.assert_called_once()
             self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
             self.assertIsNotNone(data)
 
-    def test_post_animal_when_valid(self, animal_service_mock) -> None:
-        animal = Animal()
+    def test_delete_animal_when_id_exist(self):
 
-        def side_effect(animalJson: str):
-            return animal
+        with self.app.test_client() as c:
+            response = c.delete('/animal/{0}'.format(self.id_exist))
+            self.animal_service.delete.assert_called()
+            self.assertEqual(response.status_code, HTTPStatus.NO_CONTENT)
 
-        animal_service_mock.save.side_effect = side_effect
+    def test_delete_animal_when_id_not_exist(self):
 
-        with self.__class__.app.test_client() as c, self.__class__.app.container.animal_service.override(animal_service_mock):
-            response = c.post('/animal/', json=dict())
-
-            self.assertEqual(response.status_code, HTTPStatus.CREATED)
-
-    def test_post_animal_when_not_valid(self, animal_service_mock) -> None:
-        pass
-
-    # def test_get_animal_when_invalid_id(self, animal_service_mock) -> None:
-    #     not_valid_id = 0
-
-    #     animal_service_mock.find_by_id.side_effect = self.when_id_side_effect()
-
-    #     with self.__class__.app.test_client() as c, self.__class__.app.container.animal_service.override(animal_service_mock):
-
-    #         response = c.get('/animal/{0}'.format(not_valid_id))
-    #         data = response.data
-    #         self.assertEqual(response.status_code,
-    #                          HTTPStatus.INTERNAL_SERVER_ERROR)
-    #         self.assertIsNotNone(data)
+        with self.app.test_client() as c:
+            response = c.delete('/animal/{0}'.format(self.id_does_not_exist))
+            self.animal_service.delete.assert_called()
+            self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
 
 if __name__ == '__main__':
