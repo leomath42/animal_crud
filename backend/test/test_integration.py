b3122b4d12a8274d7bfd3569a7522f709fb4162b
leomath42 <leonardomatheus042@gmail.com>
2021-11-25 08:38:22 -0300

    update

--- backend/test/test_integration.py
+++ backend/test/test_integration.py
@@ -6,6 +6,7 @@ from app.resource.resource import animal
 from flask import Flask, request, jsonify
 from app import create_app, registre_blueprints, init_database
 from app.model.model import Animal
+from mongoengine.document import Document
 
 from werkzeug.test import Client
 from werkzeug.testapp import test_app
@@ -16,35 +17,43 @@ from bson.objectid import ObjectId
 
 class Test(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.app = create_app()
-        registre_blueprints(cls.app)
-        init_database(cls.app)
+    def setUp(self) -> None:
+        self.app = create_app()
+        registre_blueprints(self.app)
+        init_database(self.app)
 
-    @classmethod
-    def tearDownClass(cls) -> None:
+        Document.save(
+            {
+                "id": "6197e0c5902bd4c1e75ba3f6",
+                "data_nascimento": "10/10/2021",
+                "nome": "dog",
+                "peso": 60,
+                "tipo": "cachorro"
+            }
+        )
+
+    def tearDown(self) -> None:
         disconnect()
 
     def test_get_animal_when_id_does_not_exist(self) -> None:
-        app = Test.app
-        with app.test_client() as c:
+
+        with self.app.test_client() as c:
             response = c.get('/animal/6197e0c5902bd4c1e75ba3f6')
             data = response.data
             self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
             self.assertIsNotNone(data)
 
     def test_get_animal_when_id_exist(self) -> None:
-        app = Test.app
-        with app.test_client() as c:
+
+        with self.app.test_client() as c:
             response = c.get('/animal/6197e0c5902bd4c1e75ba3f6')
             data = response.data
             self.assertEqual(response.status_code, HTTPStatus.OK)
             self.assertIsNotNone(data)
 
     def test_post_animal(self):
-        app = Test.app
-        with app.test_client() as c:
+
+        with self.app.test_client() as c:
             animal = {"nome": 'test', "peso": 5, "tipo": 'test',
                       "data_nascimento": '2021-11-19T00:00:00'}
             response = c.post('/animal/', json=animal)
