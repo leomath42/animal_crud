d55ee206efea606e4acf45d3690b157204ec8ae0
leomath42 <leonardomatheus042@gmail.com>
2021-11-25 10:13:11 -0300

    Seed for database in integration test

--- backend/test/test_integration.py
+++ backend/test/test_integration.py
@@ -10,7 +10,7 @@ from mongoengine.document import Document
 
 from werkzeug.test import Client
 from werkzeug.testapp import test_app
-from mongoengine import connect, disconnect
+from mongoengine import connect, disconnect, get_connection
 from datetime import date
 from bson.objectid import ObjectId
 
@@ -22,18 +22,39 @@ class Test(unittest.TestCase):
         registre_blueprints(self.app)
         init_database(self.app)
 
-        Document.save(
-            {
-                "id": "6197e0c5902bd4c1e75ba3f6",
-                "data_nascimento": "10/10/2021",
-                "nome": "dog",
-                "peso": 60,
-                "tipo": "cachorro"
-            }
-        )
+        # SEED DATABASE INICIO
+        animal_dict_1 = {
+            "id": "619bd05b20c5f552ce95e28a",
+            "data_nascimento": "10/10/2021",
+            "nome": "Caramelo",
+            "peso": 7.0,
+            "tipo": "cachorro"
+        }
+
+        animal_dict_2 = {
+            "id": "6197e0c5902bd4c1e75ba3f6",
+            "data_nascimento": "09/10/2021",
+            "nome": "Mel",
+            "peso": 5,
+            "tipo": "gato"
+        }
+        animal_dict_3 = {
+            "id": "6197e0c5902bd4c1e75ba3f7",
+            "data_nascimento": "08/10/2021",
+            "nome": "cusco",
+            "peso": 0.2,
+            "tipo": "hamster"
+        }
+        animal_1 = Animal(**animal_dict_1)
+        animal_2 = Animal(**animal_dict_2)
+        animal_3 = Animal(**animal_dict_3)
+        Animal.objects.insert([animal_1, animal_2, animal_3])
+        # SEED DATABASE FIM
 
     def tearDown(self) -> None:
-        disconnect()
+        from app import database
+        con = database.get_connection()
+        con.drop_database('test')
 
     def test_get_animal_when_id_does_not_exist(self) -> None:
 
